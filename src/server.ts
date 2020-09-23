ee8de321576e28f758d84c2628f4fa72f7cc303e
Pok Lee <pok.lee@gmail.com>
2020-09-22 20:32:27 -0400

    Update msg

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import express from 'express';
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.get('/', (req : any, res : any) => res.send('Hello World!'));
+app.get('/', (req : any, res : any) => res.send('Hello World from GitHub!'));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
